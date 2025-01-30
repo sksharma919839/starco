b4d8e1812dd401ce5fd897cb6c0e4f3bfb06730f
Sunil Sharma <165309138+sksharma919839@users.noreply.github.com>
2025-01-30 14:15:07 +0530

    Update Holders.jsx

--- src/Components/Holder/Holders.jsx
+++ src/Components/Holder/Holders.jsx
@@ -33,7 +33,7 @@ function Holders() {
           data-aos-delay="100"
           className="flex items-center justify-center py-[30px]"
         >
-          <h2 className="hover:text-[#C88887] text-[32px] cursor-pointer text-[#ffffff] transition-all duration-500 text-center">
+          <h2 className="hover:text-[#bf8cff] text-[32px] cursor-pointer text-[#ffffff] transition-all duration-500 text-center">
             Supported Accessories
           </h2>
         </div>
