9385547a6d729e75cc533707d3d3bd126018530d
Sunil Sharma <165309138+sksharma919839@users.noreply.github.com>
2025-01-30 14:18:05 +0530

    Update Metros.jsx

--- src/Components/Metro/Metros.jsx
+++ src/Components/Metro/Metros.jsx
@@ -15,7 +15,7 @@ function Metros() {
           data-aos-delay="100"
           className="flex items-center justify-center py-[30px]"
         >
-          <h2 className="hover:text-[#C88887] text-[32px] cursor-pointer text-[#ffffff] transition-all duration-500 text-center">
+          <h2 className="hover:text-[#bf8cff] text-[32px] cursor-pointer text-[#ffffff] transition-all duration-500 text-center">
             METRO
           </h2>
         </div>
