1acdac42674ce9935cfcc672c5670d30c5c6ef99
Sunil Sharma <165309138+sksharma919839@users.noreply.github.com>
2025-01-30 14:19:07 +0530

    Update Rios.jsx

--- src/Components/Rio/Rios.jsx
+++ src/Components/Rio/Rios.jsx
@@ -23,7 +23,7 @@ function Rios() {
           data-aos-delay="100"
           className="flex items-center justify-center py-[30px]"
         >
-          <h2 className="hover:text-[#C88887] text-[32px] cursor-pointer text-[#ffffff] transition-all duration-500">
+          <h2 className="hover:text-[#bf8cff] text-[32px] cursor-pointer text-[#ffffff] transition-all duration-500">
             Rio Switches
           </h2>
         </div>
